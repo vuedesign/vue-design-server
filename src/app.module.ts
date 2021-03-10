69977702909f95d18681899ad1dc46ca19fad355
n.see <gooarter@gmail.com>
2021-03-11 00:03:00 +0800

    项目编辑

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import appsModule from './apps/imports';
 import { JwtAuthGuard } from './apps/auth/guards/jwt-auth.guard';
-// import { MulterModule } from '@nestjs/platform-express';
 import { FileModule } from './apps/file/file.module';
 
 @Module({
